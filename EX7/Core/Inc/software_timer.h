1b9c652891d8f99572e5d9d7d60da8635e426858
truongtran19315 <truong.chiptttran@gmail.com>
2023-10-29 14:10:10 +0700

    EX7

---
+++ EX7/Core/Inc/software_timer.h
@@ -0,0 +1,20 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 29, 2023
+ *      Author: truon
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int TIMER_CYCLE;
+extern int timer0_flag;
+extern int timer1_flag;
+
+void setTimer0(int duration);
+void setTimer1(int duration);
+
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
